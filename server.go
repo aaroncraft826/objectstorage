424cf769069039571cee0ff15b12a7cea4dba5eb
aaroncraft826 <ag.icecream@gmail.com>
2021-10-06 15:48:13 -0400

    more testing

--- server.go
+++ server.go
@@ -3,9 +3,21 @@ package objectstorage
 import "fmt"
 
 type Server struct {
-	port int
+	port    int
+	running bool
 }
 
-func (s Server) start() {
+func (s Server) Start(port int) {
+	if s.running {
+		fmt.Println("ERROR: Server already running")
+		return
+	}
+
+	s.port = port
+	s.running = true
 	fmt.Println("Hello world! This is the Server")
+}
+
+func (s Server) Stop() {
+	s.running = false
 }
